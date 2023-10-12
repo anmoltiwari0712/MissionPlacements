fadd8d0c16c4672ee350a81b95adc0aa90f022fe
Anmol Tiwari <oanmoltiwari7@gmail.com>
2023-10-12 20:09:45 +0530

    Delete 1. Learn The Basics/1.1 Things to Know in C++JavaPython or any language directory

--- 1. Learn The Basics/1.1 Things to Know in C++JavaPython or any language/userinputoutput.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<bits/stdc++.h>//includes all the libraries supporting cpp
-using namespace std; //using namespace is used to remove std::cout 
-int main(){
-    int num;
-    cin>>num;
-    cout<<"The Number You Have Entered Is: "<<num<<endl;
-
-}

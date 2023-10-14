4f15109459941953eb9757b5ee0848615eed9d14
Anmol Tiwari <oanmoltiwari7@gmail.com>
2023-10-14 20:49:32 +0530

    Add files via upload

---
+++ 1. Learn The Basics/1.1 Things to Know in C++JavaPython or any language/functions.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int sumof2(int num1,int num2){
+    return num1+num2;
+}
+int main(){
+    int num1,num2;
+    cin>>num1>>num2;
+    cout<<sumof2(num1,num2);
+}

572ff82e4cf4c4308b9dd12f2dfb82fb2e22f63f
Anmol Tiwari <oanmoltiwari7@gmail.com>
2023-10-15 20:14:00 +0530

    Add files via upload

---
+++ 1. Learn The Basics/1.1 Things to Know in C++JavaPython or any language/Pattern Questions/pattern4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+/*
+1
+2 2
+3 3 3
+*/
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}

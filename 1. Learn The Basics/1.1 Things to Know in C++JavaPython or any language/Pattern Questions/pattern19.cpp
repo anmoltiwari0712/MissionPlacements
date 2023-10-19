19d31fbc6cb3f2c6b8b21150bf110e731aac9e18
Anmol Tiwari <oanmoltiwari7@gmail.com>
2023-10-19 16:23:50 +0530

    Add files via upload

---
+++ 1. Learn The Basics/1.1 Things to Know in C++JavaPython or any language/Pattern Questions/pattern19.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+/*
+********
+***  ***
+**    **
+*      *
+        
+*      *
+**    **
+***  ***
+********
+*/
+int main(){
+    int n;
+    cin>>n;
+    int inis=0;
+    for(int i=0;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<inis;j++){
+            cout<<" ";
+
+        }
+        for(int j=1;j<=n-i;j++){
+            cout<<"*";
+        }
+        inis=inis+2;
+        cout<<endl;
+    }
+    inis=2*n-2;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<inis;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        inis=inis-2;
+        cout<<endl;
+    }
+}

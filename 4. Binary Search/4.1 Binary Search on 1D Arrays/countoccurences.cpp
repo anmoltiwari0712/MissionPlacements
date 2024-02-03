223133c53486fc331c5cc0bc98c7cf5164b7853f
Anmol Tiwari <oanmoltiwari7@gmail.com>
2024-02-03 22:57:44 +0530

    Add files via upload

---
+++ 4. Binary Search/4.1 Binary Search on 1D Arrays/countoccurences.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+int firstoccurrence(vector<int>& arr, int n, int x){
+	int low=0;
+	int high=n-1;
+	int first=-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]==x){
+			first=mid;
+			high=mid-1;
+		}
+		else if(arr[mid]<x){
+			low=mid+1;
+		}
+		else{
+			high=mid-1;
+		}
+	}
+	return first;
+}
+int lastoccurrence(vector<int>& arr, int n, int x){
+	int low=0;
+	int high=n-1;
+	int last=-1;
+	while(low<=high){
+		int mid=(low+high)/2;
+		if(arr[mid]==x){
+			last=mid;
+			low=mid+1;
+		}
+		else if(arr[mid]<x){
+			low=mid+1;
+		}
+		else{
+			high=mid-1;
+		}
+	}
+	return last;
+}
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k) {
+    int first = firstoccurrence(arr, n, k);
+    if (first == -1) return { -1, -1};
+    int last = lastoccurrence(arr, n, k);
+    return {first, last};
+}
+int main(){
+    int n; 
+    cin>>n;
+    vector<int> arr;
+    int ele;
+    for(int i=0;i<n;i++){
+        cin>>ele;
+        arr.push_back(ele);
+    }
+    int x;
+    cin>>x;
+    // Brute Force - T.C - O(N)
+    int count=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            count++;
+        }
+    }
+    cout<<count;
+    // Optimal Force - T.C - O(N)
+
+    pair<int, int> ans = firstAndLastPosition(arr, n, x);
+    if (ans.first == -1) return 0;
+    return (ans.second - ans.first + 1);
+
+}
